ec7a926f63406c07215c0066e0d45a2a14f9ad81
Victor Rodzko <vicrodzko@mail.ru>
2018-09-12 13:06:07 +0300

    searh love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,32 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+  let arrHelp = [];
+
+  preferences.unshift("Пустота");
+
+  for (let i = 1; i < preferences.length; i++) {
+    if (i == preferences[preferences[preferences[i]]]) {	
+      arrHelp = [...arrHelp,
+                  preferences[i], 
+                  preferences[preferences[i]], 
+                  preferences[preferences[preferences[i]]]
+                ];
+    }
+  };
+
+  arrHelp = arrHelp.sort(function(a, b) {
+    return a - b;
+  })
+
+  for (let k = 0; k < arrHelp.length; k++) {
+    if (arrHelp[k] == arrHelp[k+1]) {
+      arrHelp.splice(k, 1);
+      k = -1;
+    }
+  };
+
+  let count = Math.floor(arrHelp.length/3);
+  return count;
 };
